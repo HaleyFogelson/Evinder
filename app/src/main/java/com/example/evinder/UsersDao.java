41c5a63c18b7800b48fbabb7a4182dfed7360b75
Damien Gens <damien.gens@insa-rouen.fr>
2022-03-24 16:35:13 +0100

    Creation of the 3 DAOs, AppDatabase is coming

---
+++ app/src/main/java/com/example/evinder/UsersDao.java
@@ -0,0 +1,29 @@
+package com.example.evinder;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+import java.util.List;
+
+@Dao
+public interface UsersDao {
+    @Query("SELECT * FROM Users")
+    List<Users> getAllUsers();
+
+    @Query("SELECT * FROM Users WHERE user_id=:user_id")
+    Users getUserById(int user_id);
+
+    @Query("SELECT * FROM Users WHERE name=:name")
+    List<Users> getUsersByName(String name);
+
+    @Insert
+    long insert(Users user);
+
+    @Update
+    public void update(Users user);
+
+    @Delete
+    void delete(Users user);
+}
