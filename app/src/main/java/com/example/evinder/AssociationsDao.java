ea093469292f5b6bb75778802f691427358c30b6
Paulo7699 <paul.bridier@insa-rouen.fr>
2022-04-21 15:29:38 +0200

    this works

--- app/src/main/java/com/example/evinder/AssociationsDao.java
+++ app/src/main/java/com/example/evinder/AssociationsDao.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 @Dao
 public interface AssociationsDao {
+    /**
     @Query("SELECT * FROM Associations")
     List<Associations> getAllAssociations();
 
@@ -17,6 +18,11 @@ public interface AssociationsDao {
 
     @Query("SELECT * FROM Associations WHERE user_id_assoc=:user_id_assoc")
     List<Associations> getAssociationsForUser(int user_id_assoc);
+
+
+    @Query("SELECT * FROM Associations WHERE event_id_assoc=:event_id_assoc")
+    List<Users> getUsersForAssociation(int event_id_assoc);
+     */
     
     @Insert
     long insert(Associations association);
